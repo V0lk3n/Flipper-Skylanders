4aa3f833a1cc1258f1d2f80632df9b4c84a6f7a5
V0lk3n <22322762+V0lk3n@users.noreply.github.com>
2022-07-30 15:34:38 +0200

    First Upload

---
+++ Readme.md
@@ -0,0 +1,49 @@
+# Flipper Skylanders
+
+This repository store Skylanders Keys to read sector and data of a Skylanders NFC Toys.
+
+Currently under testing.
+
+
+## How to retrieve the 16 keys 
+
+First we need to read the Skylanders NFC Toy to retrieve its UID, using Flipper Zero or an app that allow you to do that.
+
+
+
+Then, we need to use the NFC Toys Script <a href="https://nfc.toys/interop-sky.html">"tnp3xxx.py"</a>, to generate the 16 keys which allow us to read sector and data. You can also find it hosted on this repository <a href="scripts/tnp3xxx.py">here</a>
+
+```
+┌──(v0lk3n㉿lab)-[~]
+└─$ python2 tnp3xxx.py 2453be1f
+4b0b20107ccb
+ba50b997d1b8
+9c3d6dc4067b
+0f0b876ded9a
+43d12fca431d
+d0e7c563a8fc
+f68a11307f3f
+65bcfb9994de
+fd08aad6c9d1
+6e3e407f2230
+4853942cf5f3
+db657e851e12
+97bfd622b095
+04893c8b5b74
+22e4e8d88cb7
+b1d202716756
+```
+
+Then add those 16 keys to you'r "/nfc/assets/mf_classic_dict_user.nfc" file.
+
+I'm adding my keys for each of my own Skylanders Toys into this file <a href="/nfc/assets/skylanders_dict.nfc">here</a>. Feel free to contribute for skylanders owner! 
+
+At the moment of writing, the flipper zero ignore the tag and doesnt run the dictionnary attack, but a pull request was mad yesterday to implement it, feel free to follow it : https://github.com/flipperdevices/flipperzero-firmware/pull/1497
+
+## Ressources 
+
+<a href="https://nfc.toys/interop-sky.html">[NFC Toys] Skylanders Interoperability</a>
+<a href="https://nfc.toys/data-giants.html">[NFC Toys] Skylanders Data Analyse</a>
+<a href="https://nfc.toys/prac-keys.html">[NFC Toys] Key Generation</a>
+<a href="">[NFC Toys] Writing Your Own Data to Skylanders</a>
+<a href="http://con-mod.com/skylanders-nfc/">[Con Mod] Clonning Skylanders NFC !!! NO HTTPS</a>
---
+++ scripts/tnp3xxx.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+
+## tnp3xxx.py - Compute a key A
+##
+## Written in 2016 and 2017 and 2018 by Vitorio Miliano
+##
+## To the extent possible under law, the author has dedicated all
+## copyright and related and neighboring rights to this software to
+## the public domain worldwide.  This software is distributed without
+## any warranty.
+##
+## You should have received a copy of the CC0 Public Domain
+## Dedication along with this software.  If not, see
+## <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+import binascii, re, struct, sys
+
+uidre = re.compile('^[0-9a-f]{8}$', re.IGNORECASE)
+magic_nums = [2, 3, 73, 1103, 2017, 560381651, 12868356821]
+
+# Standard MSB CRC pseudocode e.g. https://en.wikipedia.org/w/index.php?title=Computation_of_cyclic_redundancy_checks&oldid=771577830#Bit_ordering_.28endianness.29
+# CRC64 ECMA-182 e.g. http://stackoverflow.com/a/29241216
+def pseudo_crc48(crc, data):
+    POLY = 0x42f0e1eba9ea3693
+    MSB = 0x800000000000
+    TRIM = 0xffffffffffff
+    for x in data:
+        crc = crc ^ (x << 40)
+        for k in range(0, 8):
+            if crc & MSB:
+                crc = (crc << 1) ^ POLY
+            else:
+                crc = crc << 1
+            crc = crc & TRIM
+    return crc
+
+def calc_keya(uid, sector):
+    if sector == 0:
+        return format(magic_nums[2] * magic_nums[4] * magic_nums[5], '012x')
+
+    if uidre.match(uid) is None:
+        raise ValueError('invalid UID (four hex bytes)')
+
+    if sector < 0 or sector > 15:
+        raise ValueError('invalid sector (0-15)')
+
+    PRE = magic_nums[0] * magic_nums[0] * magic_nums[1] * magic_nums[3] * magic_nums[6]
+    ints = [ord(a) for a in uid.decode('hex')] + [sector]
+
+    key = pseudo_crc48(PRE, ints)
+
+    return binascii.hexlify(struct.pack('<Q', key))[0:12]
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        keysa = []
+        for sector in range(0, 16):
+            keysa.append(calc_keya(sys.argv[1], sector))
+        if len(sys.argv) > 2 and sys.argv[2] == '-eml':
+            print ('0'*20+'\n'+('0'*32+'\n')*3).join(keysa).join([(sys.argv[1]+'0'*24+'\n')+(('0'*32+'\n')*2), '0'*20])
+        else:
+            print '\n'.join(keysa)
